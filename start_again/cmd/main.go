df2faef96d2aba104c2a45e4ddef5bac2379e16f
venir.dev <luca@venir.dev>
2023-09-22 07:15:41 +0200

    Playing with slices. Again.

--- start_again/cmd/main.go
+++ start_again/cmd/main.go
@@ -14,4 +14,15 @@ func main() {
 		backTicks := `Hello world`
 		fmt.Println(backTicks)
 	}
+	arr := [...]uint16{1, 2, 3}
+	fmt.Println(arr)
+	slice := []uint16{1, 2, 3}
+	anotherSlice := append(slice, 0)
+	fmt.Println(slice)
+	fmt.Println(anotherSlice)
+	spread := make([]uint16, len(slice), 2*cap(slice))
+	copy(spread, slice)
+	fmt.Println(spread)
+	spread = append(spread, anotherSlice...)
+	fmt.Println(spread)
 }
